45b468cd226bf7f58f781cff1652eba9c9120d5e
Remy CASTRO <remycastro27@icloud.com>
2023-12-12 09:59:52 +0100

    reset project

---
+++ src/main/java/com/miagem2/cinema_booking/CinemaBookingApplication.java
@@ -0,0 +1,13 @@
+package com.miagem2.cinema_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miagem2/cinema_booking/CinemaBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miagem2.cinema_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
